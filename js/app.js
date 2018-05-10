247f03e688399f3fe4f22bb842fdca689f4360fd
Krittiya Clark <krittiyaclark@gmail.com>
2018-05-10 00:48:58 -0400

    Hide overlay

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// Shuffle cards
+// Flipping cards
+// What happens when cards match
+// What happens when cards do not match
+// When the game finishes
+
+
+// Hide overlay
+function overlay() {
+	let ele =  document.querySelector(".overlay");
+
+	// if (win == false) {
+		ele.style.display = "block";
+    	ele.style.display = "none";
+	// }
+	// else {
+	// 	ele.style.display = "block";
+	// }
+}
+
+overlay();
