fb03ee514042042800d4c478df18b1aa49fed20f
Krittiya Clark <krittiyaclark@gmail.com>
2018-05-11 16:54:01 -0400

    Set startTimer function

--- js/app.js
+++ js/app.js
@@ -2,77 +2,112 @@
 let card = document.querySelectorAll(".card");
 // Select deck
 let deck = document.querySelector(".deck");
+// Select timer
+let timer = document.querySelector(".timer");
+// Choice array
+let choice = [];
+// Set match
+let match = 0;
+// User stars
+let stars = 3;
+// User move
+let move = 0;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+function load() {
+	startTimer();
+	function startTimer() {
+		let date = new Date();
+		let sec = date.getSeconds();
+		let min = date.getMinutes();
 
-function displayCards() {
-	// Shuffle function from http://stackoverflow.com/a/2450976
-	function shuffle(array) {
-	    var currentIndex = array.length, temporaryValue, randomIndex;
-
-	    while (currentIndex !== 0) {
-	        randomIndex = Math.floor(Math.random() * currentIndex);
-	        currentIndex -= 1;
-	        temporaryValue = array[currentIndex];
-	        array[currentIndex] = array[randomIndex];
-	        array[randomIndex] = temporaryValue;
-	    }
-
-	    return array;
+		if (sec++ === 60) {
+			sec = 0;
+			if (min++ === 60) {
+				min = 0;
+			}
+		}
+		timer.innerHTML = min + " : " + sec;
+		setInterval(startTimer, 1000);
 	}
-	
-	// Loop through each card and create its HTML
-	function cardIcons() {
-	        let cardFaces = [
-	            "fa-diamond",
-	            "fa-paper-plane-o",
-	            "fa-anchor",
-	            "fa-bolt",
-	            "fa-cube",
-	            "fa-leaf",
-	            "fa-bicycle",
-	            "fa-bomb",
-	            "fa-diamond",
-	            "fa-paper-plane-o",
-	            "fa-anchor",
-	            "fa-bolt",
-	            "fa-cube",
-	            "fa-leaf",
-	            "fa-bicycle",
-	            "fa-bomb"
-	        ];
 
-	        // Add each card's HTML to the page
-		    console.log(shuffle(cardFaces)); 
-		    cardFaces = shuffle(cardFaces); 
-	        cardUi = "";
-	        for (let cardFace of cardFaces) {
-		      	cardUi += `<li class="card"><i class="fa ${cardFace}"></i></li>`;
-		    
+	function stopTimer() {
+		clearInterval(startTimer);
+		target.innerHTML = `${"0 : 0"}`;
+	}
+
+	/*
+	 * Display the cards on the page
+	 *   - shuffle the list of cards using the provided "shuffle" method below
+	 *   - loop through each card and create its HTML
+	 *   - add each card's HTML to the page
+	 */
+
+	function displayCards() {
+		// Shuffle function from http://stackoverflow.com/a/2450976
+		function shuffle(array) {
+		    var currentIndex = array.length, temporaryValue, randomIndex;
+
+		    while (currentIndex !== 0) {
+		        randomIndex = Math.floor(Math.random() * currentIndex);
+		        currentIndex -= 1;
+		        temporaryValue = array[currentIndex];
+		        array[currentIndex] = array[randomIndex];
+		        array[randomIndex] = temporaryValue;
 		    }
-  
 
-		    deck.innerHTML = cardUi;	
-		    deck.addEventListener("click", function(e) {
-		    	if(e.target && e.target.nodeName == "LI") {
-			      console.log('Color Work!');
-			      
-			      e.target.classList.add("show", "open");
+		    return array;
+		}
+		
+		// Loop through each card and create its HTML
+		function cardIcons() {
+		        let cardFaces = [
+		            "fa-diamond",
+		            "fa-paper-plane-o",
+		            "fa-anchor",
+		            "fa-bolt",
+		            "fa-cube",
+		            "fa-leaf",
+		            "fa-bicycle",
+		            "fa-bomb",
+		            "fa-diamond",
+		            "fa-paper-plane-o",
+		            "fa-anchor",
+		            "fa-bolt",
+		            "fa-cube",
+		            "fa-leaf",
+		            "fa-bicycle",
+		            "fa-bomb"
+		        ];
+
+		        // Add each card's HTML to the page
+			    console.log(shuffle(cardFaces)); 
+			    cardFaces = shuffle(cardFaces); 
+		        cardUi = "";
+		        for (let cardFace of cardFaces) {
+			      	cardUi += `<li class="card"><i class="fa ${cardFace}"></i></li>`;
+			    
 			    }
-				
-			});
-		    
+	  
+
+			    deck.innerHTML = cardUi;	
+			    deck.addEventListener("click", function(e) {
+			    	if(e.target && e.target.nodeName == "LI") {
+				      console.log('Work!');
+				      
+				      e.target.classList.add("show", "open");
+				    }
+					
+				});
+			    
+		}
+
+		cardIcons();
 	}
 
-	cardIcons();
-}
+	displayCards();
 
-displayCards();
+}
+document.addEventListener('DOMContentLoaded', load);
 // What happens when cards match
 // What happens when cards do not match
 // When the game finishes
