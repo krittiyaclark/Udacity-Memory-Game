232779ccce4486ad449f86ec036abcf69ff9f2fc
Krittiya Clark <krittiyaclark@gmail.com>
2018-05-14 18:35:08 -0400

    Change addEventListener parent

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 // Select cards
-//let card = document.querySelectorAll(".card");
+let card = document.getElementsByClassName("card");
+console.log(card);
 // Select deck
-let deck = document.querySelector(".deck");
+const deck = document.getElementsByClassName("deck");
+console.log(deck);
 // Select timer
-let timer = document.querySelector(".timer");
+let timer = document.getElementsByClassName("timer");
+console.log(timer);
 // Select move
-const move = document.querySelector('.moves');
+let move = document.getElementsByClassName('moves');
+console.log(move);
 // Select stars
 let star = document.querySelectorAll('.stars li i');
+//console.log(stars li i);
 // Open cards array
 let openCards = [];
 // Set match
@@ -24,51 +29,49 @@ let cardFaces = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-c
 
 
 
+window.onload = function () {
+	playGame();
+		/*
+		 * Display the cards on the page
+		 *   - shuffle the list of cards using the provided "shuffle" method below
+		 *   - loop through each card and create its HTML
+		 *   - add each card's HTML to the page
+		 */
 
-playGame();
-	/*
-	 * Display the cards on the page
-	 *   - shuffle the list of cards using the provided "shuffle" method below
-	 *   - loop through each card and create its HTML
-	 *   - add each card's HTML to the page
-	 */
-
-// Loop through each card and create its HTML
-function cardIcons() {
-	// Clear deck
-	deck.innerHTML = "";
-    // Add each card's HTML to the page
-    cardUi = "";
-    for (let cardFace of cardFaces) {
-        cardUi += `<li class="card"><i class="fa ${cardFace}"></i></li>`;
-    }
-    deck.innerHTML = cardUi;
-}
-	
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-		
-// Event lister to deck
-deck.addEventListener("click", function(e) {
-	startTimer();
-	if(e.target.classList.contains("open")) {
-		return;
+	// Loop through each card and create its HTML
+	function cardIcons() {
+		// Clear deck
+		deck.innerHTML = "";
+	    // Add each card's HTML to the page
+	    cardUi = "";
+	    for (let cardFace of cardFaces) {
+	        cardUi += `<li class="card"><i class="fa ${cardFace}"></i></li>`;
+	    }
+	    deck.innerHTML = cardUi;
 	}
-	// Check if moves = 2 
-	if(clicks < 2) {
-		if(e.target && e.target.nodeName === "LI") {
+		
+	// Shuffle function from http://stackoverflow.com/a/2450976
+	function shuffle(array) {
+	    var currentIndex = array.length, temporaryValue, randomIndex;
+
+	    while (currentIndex !== 0) {
+	        randomIndex = Math.floor(Math.random() * currentIndex);
+	        currentIndex -= 1;
+	        temporaryValue = array[currentIndex];
+	        array[currentIndex] = array[randomIndex];
+	        array[randomIndex] = temporaryValue;
+	    }
+
+	    return array;
+	}
+			
+	// Event lister to deck
+	document.addEventListener("click", function(e) {
+		startTimer();
+		if(e.target.classList.contains("open")) {
+			return;
+		}		
+		if(e.target.classList.contains('card') && e.target.nodeName === "LI") {
 	        console.log('Work!');
 	        clicks += 1;
 	        e.target.classList.add("show");
@@ -80,6 +83,8 @@ deck.addEventListener("click", function(e) {
 		    	moveCount();
 		    }
 	    }
+
+
 	    if(openCards.length === 2) {
 		    if(openCards[0] === openCards[1]) {
 		    	e.target.classList.add("show");
@@ -97,74 +102,74 @@ deck.addEventListener("click", function(e) {
 		    }
 		}
 
+		
+
+		
+	});
+
+	function matchCount() {
+		match += 1;
 	}
 
-	
-});
+	function moveCount() {
+	    moves += 1;
+	    move.innerHTML = moves;
 
-function matchCount() {
-	match += 1;
-}
+	    for (let i = 0; i < star.length; i++) {
+	    	if(moves === 15) {
+	    		let threeStars = star -= 1;
+	        }
+	        else if(moves === 30) {
+	    		let threeStars = star -= 1;
+	        }
+	    }
+	}
 
-function moveCount() {
-    moves += 1;
-    move.innerHTML = moves;
+	// PlayGame: Call most functions here
+	function playGame() {
+		//startTimer();
+		shuffle(cardFaces); 
+		//console.log(shuffle(cardList));
+		//addIcon();
+		matchCount();
+		moveCount();
+		move.innerHTML = moves;
+		
+	}
 
-    for (let i = 0; i < star.length; i++) {
-    	if(moves === 15) {
-    		let threeStars = star -= 1;
-        }
-        else if(moves === 30) {
-    		let threeStars = star -= 1;
-        }
-    }
-}
+	function reset() {
+		playGame();
+		timer.innerHTML = 0;
+		moves = 0;
+		move.innerHTML = 0;
+		openCards = [];
+		match = 0;
+		threeStars = 3;
+		click = 0;
+		openCards = [];
+	}
+	// Start timer function
+	function startTimer() {
+		let date = new Date();
+		let sec = date.getSeconds();
+		let min = date.getMinutes();
 
-// PlayGame: Call most functions here
-function playGame() {
-	//startTimer();
-	shuffle(cardFaces); 
-	console.log(shuffle(cardFaces));
-	cardIcons();
-	matchCount();
-	moveCount();
-	move.innerHTML = moves;
-	
-}
-
-function reset() {
-	playGame();
-	timer.innerHTML = 0;
-	moves = 0;
-	move.innerHTML = 0;
-	openCards = [];
-	match = 0;
-	threeStars = 3;
-	click = 0;
-	openCards = [];
-}
-// Start timer function
-function startTimer() {
-	let date = new Date();
-	let sec = date.getSeconds();
-	let min = date.getMinutes();
-
-	if (sec++ === 60) {
-		sec = 0;
-		if (min++ === 60) {
-			min = 0;
+		if (sec++ === 60) {
+			sec = 0;
+			if (min++ === 60) {
+				min = 0;
+			}
 		}
+		timer.innerHTML = min + " : " + sec;
+		setInterval(startTimer, 1000);
 	}
-	timer.innerHTML = min + " : " + sec;
-	setInterval(startTimer, 1000);
-}
 
-// Stop timer function 
-function stopTimer() {
-	clearInterval(startTimer);
-	target.innerHTML = `${"0 : 0"}`;
-}
-
+	// Stop timer function 
+	function stopTimer() {
+		clearInterval(startTimer);
+		target.innerHTML = `${"0 : 0"}`;
+	}
+};
 // Show/Hide overlay
 // function won() {
 // 	let ele =  document.querySelector(".overlay");
